0375393466cb0059815e23317dcc8d5172b56511
lucasdjalma <rhino2057@gmail.com>
2025-10-23 09:54:41 -0300

    Acrescentado comenttarios aos posts

--- src/main/java/com/workshopmongo/domain/Post.java
+++ src/main/java/com/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.workshopmongo.dto.AuthorDTO;
+import com.workshopmongo.dto.CommentDTO;
 
 @Document 
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -76,6 +81,13 @@ public class Post implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -101,7 +113,6 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-	
 	
 	}
 	
